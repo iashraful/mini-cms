bafe3830ffa172cf7bfca27b42df60163e9552a5
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-04-12 13:41:03 +0600

    * Updated allowed hosts

--- blackbox/project_base/settings.py
+++ blackbox/project_base/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspa
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!8ybjp%i5a29&#gg+)b6cj-8ijfokvp%prrd0#dm((t-fj=w9w'
 
-ALLOWED_HOSTS = ['localhost', 'ashrafulrobin.pythonanywhere.com', 'mrrobin.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '.mrrobin.me', 'mrobin.me', 'cms.mrrobin.me']
 
 # Application definition
 
