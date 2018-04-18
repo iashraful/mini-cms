2fbfb94378cea7af2f88d3ddc0034aee0dfe0589
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-04-18 23:30:05 +0600

    * Added loading on page details view * moved page list view to settings dir of component

--- frontend/components/pages/PageDetails.vue
+++ frontend/components/pages/PageDetails.vue
@@ -1,27 +1,30 @@
 <template>
     <div>
-        <h5>{{ currentPage.name }}</h5>
-        <hr class="mt-0"/>
-        <h6>1. Post Title</h6>
-        <p class="text-muted mb-0">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
-        <h6 class="mt-3">2. Post Title 2</h6>
-        <p class="text-muted mb-0">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
+        <div v-if="loading" class="mt-5 text-center" style="font-size: 32px">Loading...</div>
+        <div v-if="!loading">
+            <h5>{{ currentPage.name }}</h5>
+            <hr class="mt-0"/>
+            <h6>1. Post Title</h6>
+            <p class="text-muted mb-0">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
+            <h6 class="mt-3">2. Post Title 2</h6>
+            <p class="text-muted mb-0">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
+        </div>
     </div>
 </template>
 
@@ -30,16 +33,19 @@
         name: "page-details",
         data() {
             return {
-                currentPage: this.$store.getters.getCurrentPage
+                currentPage: this.$store.getters.getCurrentPage,
+                loading: false,
             }
         },
         methods: {
             getPageDetails() {
+                this.loading = true;
                 const path = this.$route.params.pageSlug;
                 this.$store.dispatch('getPageDetailsApiCall', path).then((response) => {
                     if (response.path !== path) {
                         this.$router.push('/404')
                     }
+                    this.loading = false;
                     this.currentPage = response;
                 })
             }
--- frontend/components/settings/admin/pages/UserPageDetails.vue
+++ frontend/components/settings/admin/pages/UserPageDetails.vue
@@ -1,49 +1,45 @@
 <template>
     <div>
-        <h5>Add New Content</h5>
-        <hr class="mt-0"/>
-        <div class="mt-2 mb-4">
-            <form>
-                <label>Title</label>
-                <input type="text" class="form-control" v-model="newContent.title"/>
-                <label>Description</label>
-                <vue-editor v-model="newContent.body"></vue-editor>
-            </form>
+        <div v-if="loading" class="mt-5 text-center" style="font-size: 32px">Loading...</div>
+        <div v-if="!loading">
+            <h5>Add New Content</h5>
+            <hr class="mt-0"/>
+            <div class="mt-2 mb-4">
+                <form>
+                    <label>Title</label>
+                    <input type="text" class="form-input" v-model="newContent.title"/>
+                    <label>Description</label>
+                    <vue-editor v-model="newContent.body"></vue-editor>
+                </form>
+            </div>
+            <h5>{{ currentPage.name }} Contents</h5>
+            <hr class="mt-0"/>
+            <div>
+                <h6>1. Post Title</h6>
+                <p class="text-muted mb-0">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+                    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                    Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+                <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
+            </div>
         </div>
-        <h5>{{ currentPage.name }} Contents</h5>
-        <hr class="mt-0"/>
-        <h6>1. Post Title</h6>
-        <p class="text-muted mb-0">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
-        <h6 class="mt-3">2. Post Title 2</h6>
-        <p class="text-muted mb-0">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
     </div>
 </template>
 
 <script>
-    import { VueEditor } from 'vue2-editor'
+    import {VueEditor} from 'vue2-editor'
 
     export default {
         name: "user-page-details",
         data() {
             return {
                 currentPage: this.$store.getters.getCurrentPage,
-                newContent: {title: '', body: ''}
+                newContent: {title: '', body: ''},
+                loading: false,
             }
         },
         components: {
@@ -51,11 +47,13 @@
         },
         methods: {
             getPageDetails() {
+                this.loading = true;
                 const path = this.$route.params.pageSlug;
                 this.$store.dispatch('getPageDetailsApiCall', path).then((response) => {
                     if (response.path !== path) {
                         this.$router.push('/404')
                     }
+                    this.loading = false;
                     this.currentPage = response;
                 })
             }
@@ -75,5 +73,8 @@
 </script>
 
 <style scoped>
-
+    .form-input {
+        width: 100%;
+        height: 37px;
+    }
 </style>
