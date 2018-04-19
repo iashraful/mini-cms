d6789e36e06d5f56e60b3a90cf643f3cf968394d
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-04-19 11:20:29 +0600

    * Added checkbox for publishing a content * Rename publish button named to Save Content

--- frontend/components/settings/admin/pages/UserPageDetails.vue
+++ frontend/components/settings/admin/pages/UserPageDetails.vue
@@ -10,7 +10,9 @@
                     <input type="text" class="form-input" v-model="newContent.title" required/>
                     <label>Description</label>
                     <vue-editor v-model="newContent.body"></vue-editor>
-                    <button class="mt-2 pull-right btn btn-outline-success pl-5 pr-5">Publish</button>
+                    <button class="mt-2 pull-right btn btn-outline-success pl-5 pr-5">Save Content</button>
+                    <label class="pull-right" style="padding: 16px 21px 0 8px;">Publish</label>
+                    <input type="checkbox" class="pull-right" style="margin-top: 21px;" v-model="statusCheckBox"/>
                     <br/>
                 </form>
             </div>
@@ -19,7 +21,8 @@
             <div v-for="(content, index) in currentPage.contents" :key="index" class="mb-4">
                 <h6 class="content-title">
                     {{ index + 1 }}. {{ content.title }}
-                    <span style="font-size: 12px" :class="{'alert alert-danger p-1': content.status === enums.ContentEnum.DRAFT.value, 'alert alert-info p-1': content.status === enums.ContentEnum.ARCHIVE.value, }">
+                    <span style="font-size: 12px"
+                          :class="{'alert alert-danger p-1': content.status === enums.ContentEnum.DRAFT.value, 'alert alert-info p-1': content.status === enums.ContentEnum.ARCHIVE.value, }">
                         {{ enums.ContentEnum.props[content.status].name }}
                     </span>
                 </h6>
@@ -45,9 +48,12 @@
         data() {
             return {
                 currentPage: this.$store.getters.getCurrentPage,
-                newContent: {title: '', body: '', page_slug: this.$route.params.pageSlug},
+                newContent: {
+                    title: '', body: '', page_slug: this.$route.params.pageSlug, status: enums.ContentEnum.DRAFT.value,
+                },
                 loading: false,
-                enums: enums
+                enums: enums,
+                statusCheckBox: false
             }
         },
         components: {
@@ -80,6 +86,13 @@
             '$route'(to, from) {
                 // get current page from api
                 this.getPageDetails();
+            },
+            statusCheckBox(newVal, oldVal) {
+                if (newVal === true) {
+                    this.newContent.status = enums.ContentEnum.PUBLISH.value
+                } else {
+                    this.newContent.status = enums.ContentEnum.DRAFT.value
+                }
             }
         }
     }
