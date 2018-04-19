3b7538d76a422f018f1622ff295249912a10d8aa
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-04-19 09:15:20 +0600

    * Added content showing functionality per page

--- frontend/components/pages/PageDetails.vue
+++ frontend/components/pages/PageDetails.vue
@@ -4,26 +4,17 @@
         <div v-if="!loading">
             <h5>{{ currentPage.name }}</h5>
             <hr class="mt-0"/>
-            <h6>1. Post Title</h6>
-            <p class="text-muted mb-0">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
-            <h6 class="mt-3">2. Post Title 2</h6>
-            <p class="text-muted mb-0">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
+            <div v-for="(content, index) in currentPage.contents" :key="index">
+                <h6 class="content-title">{{ index + 1 }}. {{ content.title }}</h6>
+                <p v-if="content.body.length > 500" class="text-muted mb-0 ml-4"
+                   v-html="content.body.slice(0, 500) + '...'"></p>
+                <p v-if="content.body.length <= 500" class="text-muted mb-0 ml-4"
+                   v-html="content.body.slice(0, 500)"></p>
+                <button v-if="content.body.length > 500"
+                        class="ml-4 btn btn-outline-primary pt-0 pb-0 pl-5 pr-5">
+                    View Details
+                </button>
+            </div>
         </div>
     </div>
 </template>
--- frontend/components/settings/admin/pages/UserPageDetails.vue
+++ frontend/components/settings/admin/pages/UserPageDetails.vue
@@ -16,17 +16,16 @@
             </div>
             <h5>{{ currentPage.name }} Contents</h5>
             <hr class="mt-0"/>
-            <div>
-                <h6>1. Post Title</h6>
-                <p class="text-muted mb-0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-                    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                    Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <button class="btn btn-outline-primary pt-1 pb-1 pl-5 pr-5">View Details</button>
+            <div v-for="(content, index) in currentPage.contents" :key="index">
+                <h6 class="content-title">{{ index + 1 }}. {{ content.title }}</h6>
+                <p v-if="content.body.length > 500" class="text-muted mb-0 ml-4"
+                   v-html="content.body.slice(0, 500) + '...'"></p>
+                <p v-if="content.body.length <= 500" class="text-muted mb-0 ml-4"
+                   v-html="content.body.slice(0, 500)"></p>
+                <button v-if="content.body.length > 500"
+                        class="ml-4 btn btn-outline-primary pt-0 pb-0 pl-5 pr-5">
+                    View Details
+                </button>
             </div>
         </div>
     </div>
@@ -84,7 +83,7 @@
         width: 100%;
         height: 37px;
     }
-    
+
     .pull-right {
         float: right;
     }
