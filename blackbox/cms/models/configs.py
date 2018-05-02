0f482a014b195f16900a55104728133fd43acbff
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-05-02 22:04:27 +0600

    * Added new config field to app config * Added functionality to change browser title dynamically

--- blackbox/cms/models/configs.py
+++ blackbox/cms/models/configs.py
@@ -4,8 +4,10 @@ from blackbox.core.models.base import BaseEntity
 
 
 class AppConfig(BaseEntity):
-    app_name = models.CharField(max_length=32, default='MiNi CMS')
-    footer = models.TextField(verbose_name='App Footer', null=True)
+    app_name = models.CharField(max_length=32, default='MiNi CMS', blank=True)
+    footer = models.TextField(verbose_name='App Footer', null=True, blank=True)
+    hide_footer = models.BooleanField(default=False)
+    browser_title = models.CharField(max_length=256, default='MiNi CMS', blank=True)
 
     class Meta:
         app_label = 'cms'
--- blackbox/cms/serializers/app_configs/config_serializers.py
+++ blackbox/cms/serializers/app_configs/config_serializers.py
@@ -6,4 +6,4 @@ from blackbox.cms.models.configs import AppConfig
 class AppConfigSerializer(ModelSerializer):
     class Meta:
         model = AppConfig
-        fields = ('id', 'app_name', 'footer')
+        fields = ('id', 'app_name', 'footer', 'hide_footer', 'browser_title')
